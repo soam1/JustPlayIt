742086709ac2c826ef951e364bb421f3cae046e9
soam1 <soamakash9@gmail.com>
2023-06-15 15:35:16 +0530

    tried using previous version of exoplayer, but download button still doesn't work

--- app/src/main/java/com/akashsoam/exoplayermediaapp/MainActivity.java
+++ app/src/main/java/com/akashsoam/exoplayermediaapp/MainActivity.java
@@ -25,61 +25,73 @@ import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
     private PlayerView mPlayerView;
     private SimpleExoPlayer mSimpleExoPlayer;
-    private ProgressBar mProgressBar;
-    private FloatingActionButton mFloatingActionButton;
+    private FloatingActionButton fab;
 
-//    private Player.EventListener mEventListener;
+    // private String music_url = "https://opengameart.org/sites/default/files/the_field_of_dreams.mp3";
 
-    //    private String music_url = "https://opengameart.org/sites/default/files/the_field_of_dreams.mp3";
     String music_list[] = new String[]{
-            "https://pagalworlds.co/wp-content/uploads/Harry-Potter-Theme.mp3",
-            "https://opengameart.org/sites/default/files/the_field_of_dreams.mp3",
-            "https://opengameart.org/sites/default/files/Cyberpunk%20Moonlight%20Sonata_0.mp3",
-            "https://opengameart.org/sites/default/files/battleThemeA.mp3",
-            "https://opengameart.org/sites/default/files/Orbital%20Colossus_0.mp3",
-            "https://opengameart.org/sites/default/files/Winds%20Of%20Stories_2.mp3",
-            "https://opengameart.org/sites/default/files/fight.wav"
+            "https://pagalworlds.co/wp-content/uploads/Harry-Potter-Theme.mp3", "https://opengameart.org/sites/default/files/the_field_of_dreams.mp3", "https://opengameart.org/sites/default/files/Cyberpunk%20Moonlight%20Sonata_0.mp3", "https://opengameart.org/sites/default/files/battleThemeA.mp3", "https://opengameart.org/sites/default/files/Orbital%20Colossus_0.mp3", "https://opengameart.org/sites/default/files/Winds%20Of%20Stories_2.mp3", "https://opengameart.org/sites/default/files/fight.wav"
     };
+
+    private ProgressBar mProgressBar;
+
+    private Player.EventListener mEventListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         mPlayerView = findViewById(R.id.my_player_view);
         mProgressBar = findViewById(R.id.my_progress_bar);
 
-        mSimpleExoPlayer = new SimpleExoPlayer.Builder(this).setSeekBackIncrementMs(10000).setSeekForwardIncrementMs(10000).build();
-        mPlayerView.setPlayer(mSimpleExoPlayer);
+        mSimpleExoPlayer = new SimpleExoPlayer.Builder(this).build();
 
+        mPlayerView.setPlayer(mSimpleExoPlayer);
 
         for (String music_url : music_list) {
             MediaItem mediaItem = MediaItem.fromUri(Uri.parse(music_url));
+
             mSimpleExoPlayer.addMediaItem(mediaItem);
+
         }
-//        MediaItem mediaItem = MediaItem.fromUri(Uri.parse(music_url));
-//        mSimpleExoPlayer.setMediaItem(mediaItem);
+
+        //  mSimpleExoPlayer.setMediaItem(mediaItem);
         mSimpleExoPlayer.prepare();
         mSimpleExoPlayer.play();
 
 
-        mSimpleExoPlayer.addListener(new Player.Listener() {
+        mEventListener = new Player.EventListener() {
+
             @Override
-            public void onPlaybackStateChanged(int playbackState) {
-                Player.Listener.super.onPlaybackStateChanged(playbackState);
-                if (playbackState == Player.STATE_BUFFERING) {
+            public void onPlaybackStateChanged(int state) {
+
+                if (state == Player.STATE_BUFFERING) {
+
                     mProgressBar.setVisibility(View.VISIBLE);
-                } else if (playbackState == Player.STATE_READY) {
+
+                } else if (state == Player.STATE_READY) {
+
                     mProgressBar.setVisibility(View.GONE);
+
                 }
             }
-        });
+        };
 
-        mFloatingActionButton = findViewById(R.id.fab);
-        mFloatingActionButton.setOnClickListener(v -> {
-            setupPermissions();
+        mSimpleExoPlayer.addListener(mEventListener);
 
+
+        fab = findViewById(R.id.my_fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                setupPermissions();
+
+            }
         });
 
     }
@@ -87,13 +99,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+
         if (mSimpleExoPlayer != null) {
+
             mSimpleExoPlayer.release();
             mSimpleExoPlayer = null;
+
         }
+
     }
 
     private void setupPermissions() {
+
+
         Dexter.withContext(this)
                 .withPermissions(
                         Manifest.permission.READ_EXTERNAL_STORAGE,
@@ -101,32 +119,45 @@ public class MainActivity extends AppCompatActivity {
                 ).withListener(new MultiplePermissionsListener() {
                     @Override
                     public void onPermissionsChecked(MultiplePermissionsReport report) {
+
+
+                        //Download the current music
+
                         if (report.areAllPermissionsGranted()) {
+
                             downloadTheCurrentMusic(mSimpleExoPlayer.getCurrentMediaItem().playbackProperties.uri.toString());
-
                         }
-                    }
-                    //download the current music
 
+                    }
 
                     @Override
-                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
-                        token.continuePermissionRequest();
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken) {
+
+                        permissionToken.continuePermissionRequest();
+
                     }
                 }).check();
+
     }
 
     private void downloadTheCurrentMusic(String musicURLString) {
+
         DownloadManager downloadManager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+
         Uri uri = Uri.parse(musicURLString);
+
+
         DownloadManager.Request request = new DownloadManager.Request(uri);
-//        String musicName = musicURLString.substring(musicURLString.lastIndexOf("/" + 1));
-        String musicName = musicURLString;
+
+        String musicName = musicURLString.substring(musicURLString.lastIndexOf("/") + 1);
+
         request.setTitle(musicName);
         request.setVisibleInDownloadsUi(true);
         request.allowScanningByMediaScanner();
-        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE);
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
         request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, uri.getLastPathSegment());
         downloadManager.enqueue(request);
+
+
     }
 }
